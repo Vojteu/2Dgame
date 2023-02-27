7d7e1c0a5256254d3d459489f603a6e39e26805c
Vojteu <wojtasss821@gmail.com>
2023-02-27 13:18:00 +0100

    initial commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public float respawnDelay;
+    public PlayerController gamePlayer;
+    public int coins;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gamePlayer = FindObjectOfType<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Respawn() {
+        StartCoroutine("RespawnCoroutine");
+    }
+
+    public IEnumerator RespawnCoroutine() {
+        gamePlayer.gameObject.SetActive(false);
+        yield return new WaitForSeconds(respawnDelay);
+        gamePlayer.transform.position = gamePlayer.respawnPoint;
+        gamePlayer.gameObject.SetActive(true);
+    }
+
+    public void AddCoins(int numberOfCoins) {
+        coins += numberOfCoins;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour{
+    public float speed = 5f;
+    public float jumpSpeed = 5f;
+    private float movement = 0f;
+    private Rigidbody2D rigidBody;
+    public Transform groundCheckPoint;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    private bool isTouchingGround;
+    private Animator playerAnimation;
+    public Vector3 respawnPoint;
+    public LevelManager gameLevelManager;
+
+    //Start is called before the first frame update
+    void Start(){
+        rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.freezeRotation = true;
+        playerAnimation = GetComponent<Animator>();
+        respawnPoint = transform.position;
+        gameLevelManager = FindObjectOfType<LevelManager>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        isTouchingGround = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayer);
+        movement = Input.GetAxis ("Horizontal");
+        if(movement > 0f) {
+            rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
+            transform.localScale = new Vector2(5.2529f, 3.850944f);
+        }
+        else if(movement < 0f) {
+            rigidBody.velocity = new Vector2(movement * speed, rigidBody.velocity.y);
+            transform.localScale = new Vector2(-5.2529f, 3.850944f);
+        }
+        else{
+            rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
+        }
+
+        if (Input.GetButtonDown("Jump") && isTouchingGround) {
+           rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpSpeed);
+        }
+
+        playerAnimation.SetFloat("Speed", Mathf.Abs(rigidBody.velocity.x));
+        playerAnimation.SetBool("OnGround", isTouchingGround);
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "FallDetector") {
+            // what will happen when player enters the FallDetector zone
+            //transform.position = respawnPoint;
+            gameLevelManager.Respawn();
+        }
+        if (other.tag == "Checkpoint") {
+            respawnPoint = other.transform.position;
+        }
+    }
+}
