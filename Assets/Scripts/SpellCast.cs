1aa7893311c498df1807c680e2d8961159218ea6
Vojteu <wojtasss821@gmail.com>
2023-03-01 17:24:21 +0100

    update

---
+++ Assets/Scripts/SpellCast.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellCast : MonoBehaviour {
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    void Start(){
+        
+    }
+
+    void Update(){
+        if (Input.GetButtonDown("Fire1")) {
+            Shoot();
+        }    
+    }
+
+    void Shoot() {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+}
